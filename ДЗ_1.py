f5e86ab457d3df9571863a5f10bce6af6a4b9c50
Bulka148 <ismagilov-bulat@list.ru>
2021-09-14 22:24:06 +0300

    dz1

--- ДЗ_1.py
+++ ДЗ_1.py
@@ -2,3 +2,13 @@
 #Задание №4
 print(sorted(input())==sorted(input()))
 
+#Задание №5
+a = [1,9,9]
+x = int(''.join(map(str,a)))
+
+x = x + 1
+print()
+result = [int(i) for i in str(x)]
+
+print(result)
+
