15896805af94a52e4de4847e3ba90400903f54c2
Bulka148 <ismagilov-bulat@list.ru>
2021-09-14 23:04:52 +0300

    dzdz1

--- ДЗ_1.py
+++ ДЗ_1.py
@@ -5,10 +5,17 @@ print(sorted(input())==sorted(input()))
 #Задание №5
 a = [1,9,9]
 x = int(''.join(map(str,a)))
-
 x = x + 1
 print()
 result = [int(i) for i in str(x)]
-
 print(result)
 
+#Задание №7
+s = input()
+print(s[::-1])
+
+#Задание №8
+a = input()
+b = ''.join(i for i in a if i.isalnum())
+
+print(b[::-1] == b)
